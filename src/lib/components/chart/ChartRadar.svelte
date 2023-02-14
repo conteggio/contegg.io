5f1dbda40bfd9ea2e50efa1df18822d674aa3407
Geoff Dick <git@geoffdick.com>
2023-02-14 12:01:28 -0700

    feat: add chart.js (#4)

---
+++ src/lib/components/chart/ChartRadar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Radar } from 'svelte-chartjs';
+
+	import {
+		Chart as ChartJS,
+		Title,
+		Tooltip,
+		Legend,
+		PointElement,
+		RadialLinearScale,
+		LineElement,
+		type ChartData
+	} from 'chart.js';
+	import type { _DeepPartialObject } from 'chart.js/dist/types/utils';
+
+	ChartJS.register(Title, Tooltip, Legend, PointElement, RadialLinearScale, LineElement);
+
+	export let data: ChartData<'radar', number[], unknown>;
+	export let options: any = {
+		responsive: true
+	};
+</script>
+
+<Radar {data} {options} />
